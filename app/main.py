107a35949f07b04b8b8abd28117b480c907ed791
ana pupo <analarapupo@gmail.com>
2025-11-24 19:32:47 -0300

    implementando endpoint

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-fake_db = {
+fake_items_db = {
     1: {"id": 1, "name": "Item 1"},
-    2: {"id": 2, "name": "Item 2"}
+    2: {"id": 2, "name": "Item 2"},
+    3: {"id": 3, "name": "Item 3"},
 }
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
-    if item_id in fake_db:
-        return fake_db[item_id]
-    raise HTTPException(status_code=404, detail="Item não existe")
+    if item_id not in fake_items_db:
+        raise HTTPException(status_code=404, detail="Item não existe")
+    return fake_items_db[item_id]
